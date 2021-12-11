1cdbe6bb89786c7a7994ff26808850b1ee7069c3
Harrison <90717856+Jhegerton@users.noreply.github.com>
2021-12-11 10:21:50 -0500

    This is test three fro circle ci.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,14 +1,15 @@
 package travis_ci_tutorial_java;
 
 import java.util.*;
-
 import static org.junit.Assert.*;
 import org.junit.*;
 
+
 public class SimpleCalculatorTest {
+	final int SUM = 2;
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.add(1, 1), SUM );
 	}
 }
